271380b8555942ef5810a94b4f355b5909effed0
katushka <katya.safutina@bk.ru>
2020-05-05 07:53:34 +0000

    Revert_string

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,27 @@
 #include "revert_string.h"
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int len,i;
+    char *beg_ptr, *end_ptr, ch;
+
+    len=strlen(str);
+    beg_ptr=str;
+    end_ptr=str;
+
+    for (i=0; i<len-1; i++)
+    end_ptr++;
+
+    for (i=0; i<len/2; i++){
+        ch=*end_ptr;
+        *end_ptr=*beg_ptr;
+        *beg_ptr=ch;
+
+        beg_ptr++;
+        end_ptr--;
+    }
 }
 
